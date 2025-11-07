cc38f23a6c700fb0aff43ba24091bedde2a52355
许飞 <feixu@zhejianglab.org>
2025-11-07 13:54:07 +0800

    increase sync chn

--- substrate/client/network/src/protocol/notifications/handler.rs
+++ substrate/client/network/src/protocol/notifications/handler.rs
@@ -99,7 +99,7 @@ const LOG_TARGET: &str = "sub-libp2p::notification::handler";
 pub(crate) const ASYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 128; // Rocky: old value is 8;
 
 /// Number of pending notifications in synchronous contexts.
-const SYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 4096; // Rocky: old value is 2048;
+const SYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 8192; // Rocky: old value is 2048;
 
 /// Maximum duration to open a substream and receive the handshake message. After that, we
 /// consider that we failed to open the substream.
