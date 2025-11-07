bcc4e72a53547e02eb737d62645b765e67e87fbd
许飞 <feixu@zhejianglab.org>
2025-11-07 16:02:49 +0800

    disable propagate timeout iterval

--- substrate/client/network/transactions/src/lib.rs
+++ substrate/client/network/transactions/src/lib.rs
@@ -308,7 +308,7 @@ where
 		loop {
 			futures::select! {
 				_ = self.propagate_timeout.next() => {
-					#[cfg(not(feature = "txpool-iter-propagate-disable"))]
+					#[cfg(not(feature = "txpool-timeout-propagate-disable"))]
 					self.propagate_transactions();
 				},
 				(tx_hash, result) = self.pending_transactions.select_next_some() => {
