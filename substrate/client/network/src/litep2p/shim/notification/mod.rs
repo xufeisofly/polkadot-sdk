2b04e7439c64813d29aba45c37d9490655f842aa
许飞 <feixu@zhejianglab.org>
2025-11-10 15:01:39 +0800

    debugging

--- substrate/client/network/src/litep2p/shim/notification/mod.rs
+++ substrate/client/network/src/litep2p/shim/notification/mod.rs
@@ -82,14 +82,6 @@ impl MessageSink for Litep2pMessageSink {
 	fn send_sync_notification(&self, notification: Vec<u8>) {
 		let size = notification.len();
 
-		log::trace!(
-			target: LOG_TARGET,
-			"===2 {}: sending sync notification to {:?}, size={}",
-			self.protocol,
-			self.peer,
-			size,
-		);
-
 		match self.sink.send_sync_notification(notification) {
 			Ok(_) => self.metrics.register_notification_sent(&self.protocol, size),
 			Err(error) => log::trace!(
@@ -208,14 +200,6 @@ impl NotificationService for NotificationProtocol {
 
 	fn send_sync_notification(&mut self, peer: &PeerId, notification: Vec<u8>) {
 		let size = notification.len();
-
-		log::trace!(
-			target: LOG_TARGET,
-			"===1 {}: sending sync notification to {:?}, size={}",
-			self.protocol,
-			peer,
-			size,
-		);
 
 		if let Ok(_) = self.handle.send_sync_notification(peer.into(), notification) {
 			self.metrics.register_notification_sent(&self.protocol, size);
