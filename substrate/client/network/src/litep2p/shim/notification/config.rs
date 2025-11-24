fc0b13e0bee5f0d0a98300da410ec3cb6ad87829
许飞 <feixu@zhejianglab.org>
2025-11-24 19:40:00 +0800

    send sync log

--- substrate/client/network/src/litep2p/shim/notification/config.rs
+++ substrate/client/network/src/litep2p/shim/notification/config.rs
@@ -35,10 +35,10 @@ use sc_utils::mpsc::TracingUnboundedSender;
 
 use std::sync::{atomic::AtomicUsize, Arc};
 
-pub(crate) const ASYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 128; // Rocky: old value is 8;
+pub(crate) const ASYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 128 * 4; // Rocky: old value is 8;
 
 /// Number of pending notifications in synchronous contexts.
-const SYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 8192 * 5; // Rocky: old value is 2048; better same size as ready pool for transactions propagation.
+const SYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 8192 * 4; // Rocky: old value is 2048; better same size as ready pool for transactions propagation.
 
 /// Handle for controlling the notification protocol.
 #[derive(Debug, Clone)]
